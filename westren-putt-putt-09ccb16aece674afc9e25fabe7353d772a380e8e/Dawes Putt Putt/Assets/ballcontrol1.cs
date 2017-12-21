e7816a5c52b94850e74a35017a097c1aabc6e3cf
evynchambers <34458645+evynchambers@users.noreply.github.com>
2017-12-21 16:11:15 -0200

    first change

---
+++ westren-putt-putt-09ccb16aece674afc9e25fabe7353d772a380e8e/Dawes Putt Putt/Assets/ballcontrol1.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ballcontrol1 : MonoBehaviour {
+
+    //public Transform clubObj;
+    public float zforce = 100;
+    public Transform arrowObj;
+    private bool isShot;
+    public Rigidbody golfBall;
+    public GameObject ball;
+    private int shotsTaken;
+    public int[] pars;
+    private int parTotal;
+    private int currentHole;
+    public int[] score;
+    public GameObject[] startPos;
+    private bool inHole;
+    public GameObject[] cams;
+    public GameObject[] Hole2Cams;
+	public Slider PowerBar;
+	private bool IsGrowing;
+	private float Direction;
+   
+
+    void addPar()
+    {
+        for (int i = 0; i < pars.Length; i++)
+        {
+            parTotal += pars[i];
+        }
+    }
+
+    //score[currentHole] = pars[currentHole] - shotsTaken;
+
+	// Use this for initialization
+	void Start ()
+    {
+		IsGrowing = false;
+		inHole = false;
+        isShot = false;
+        shotsTaken = 0;
+        parTotal = 0;
+        currentHole = 0;
+		PowerBar.value = 0;
+		Direction = -.1f;
+	}
+
+	public void Right(){
+		ball.transform.Rotate(0, 3, 0);
+	}
+
+	public void Left(){
+		ball.transform.Rotate(0, -3, 0);
+	}
+
+	public void Shoot(){
+		shotsTaken++;
+		//Debug.Log(shotsTaken);
+		isShot = true;
+		ball.transform.DetachChildren();
+		//golfBall.AddRelativeForce(0, 0, zforce);
+		golfBall.AddForce(transform.forward * zforce);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (isShot == false) {
+			if (PowerBar.value == 0 ) {
+				Direction *= -1;
+
+			}
+
+
+			if (PowerBar.value == 1 ) {
+				Direction *= -1;
+				//PowerBar.value += Direction * Time.deltaTime;
+			}
+
+			PowerBar.value += Direction * Time.deltaTime;
+		}
+
+        if(golfBall.velocity == Vector3.zero)
+        {
+            if(isShot == true && inHole == false)
+            {
+                isShot = false;
+                ball.transform.position = transform.position;
+                this.transform.parent = ball.transform;
+                arrowObj.transform.parent = ball.transform;
+                golfBall.transform.rotation = ball.transform.rotation;
+                arrowObj.transform.position = transform.position;
+            }
+
+            if(isShot == true && inHole == true)
+            {
+                //Debug.Log(score[currentHole]);
+                cams[currentHole].SetActive(false);
+                score[currentHole] = pars[currentHole] - shotsTaken;
+                currentHole++;
+                ball.transform.position = startPos[currentHole].transform.position;
+                inHole = false;
+                isShot = false;
+                //ball.transform.position = transform.position;
+                this.transform.position = ball.transform.position;
+                this.transform.parent = ball.transform;
+                arrowObj.transform.parent = ball.transform;
+                golfBall.transform.rotation = ball.transform.rotation;
+                arrowObj.transform.position = transform.position;
+                cams[currentHole].SetActive(true);
+                shotsTaken = 0;
+            }
+
+        }
+
+        if (Input.GetButtonDown("up") && isShot == false)
+        {
+            shotsTaken++;
+            //Debug.Log(shotsTaken);
+            isShot = true;
+            ball.transform.DetachChildren();
+            //golfBall.AddRelativeForce(0, 0, zforce);
+            golfBall.AddForce(transform.forward * zforce);
+
+        }
+
+        //fix
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            //Instantiate(clubObj, transform.position, clubObj.rotation);
+            GetComponent<Transform>().eulerAngles = new Vector3(0, 0, 0);
+            arrowObj.GetComponent<Transform>().position = transform.position;
+        }
+
+        if (Input.GetKey("a"))
+        {
+
+            ball.transform.Rotate(0, -1, 0);
+           
+        }
+        if (Input.GetKey("d"))
+        {
+            
+            ball.transform.Rotate(0, 1, 0);
+        }
+
+        if (Input.GetButton("one"))
+        {
+            zforce += 5;
+        }
+
+        if (Input.GetButton("two"))
+        {
+            zforce -= 5;
+        }
+
+        if (zforce < 20)
+        {
+            zforce = 20;
+        }
+
+        if (zforce > 2000)
+        {
+            zforce = 2000;
+        }
+
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "cup")
+        {
+            //score[currentHole] = pars[currentHole] - shotsTaken;
+            Debug.Log("Completed");
+            inHole = true;
+            //GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if(other.name == "camera trigger")
+        {
+            if (Hole2Cams[0].activeInHierarchy)
+            {
+                Hole2Cams[0].SetActive(false);
+                Hole2Cams[1].SetActive(true);
+            }
+            else
+            {
+                Hole2Cams[1].SetActive(false);
+                Hole2Cams[0].SetActive(true);
+            }
+        }
+    }
+}
